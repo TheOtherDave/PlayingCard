9ae8adeb96aaf212628d1d974f44a2ab5fa5b779
David Sweeris <davesweeris@mac.com>
2020-02-28 12:40:17 -0800

    Initial Commit

---
+++ PlayingCard/PlayingCard.h
@@ -0,0 +1,19 @@
+//
+//  PlayingCard.h
+//  PlayingCard
+//
+//  Created by David Sweeris on 2/28/20.
+//  Copyright © 2020 David Sweeris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayingCard.
+FOUNDATION_EXPORT double PlayingCardVersionNumber;
+
+//! Project version string for PlayingCard.
+FOUNDATION_EXPORT const unsigned char PlayingCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayingCard/PublicHeader.h>
+
+
